bdbf9e55668b5605e037a39a4e7c5870270e2fd5
prithvi-076 <78290256+prithvi-076@users.noreply.github.com>
2022-11-21 14:54:35 +0530

    Add files via upload

---
+++ src/main/java/app/demo/PresentProject/App.java
@@ -0,0 +1,13 @@
+package app.demo.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
